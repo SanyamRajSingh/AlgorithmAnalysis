14f138e617de929614df39a59dccfbf234df0b2f
SanyamRajSingh <sanyamrajsingh@gmail.com>
2026-04-21 19:53:00 +0530

    P4: Quick Sort implementation

--- src/app/AlgorithmAnalysisApp.java
+++ src/app/AlgorithmAnalysisApp.java
@@ -111,4 +111,25 @@ public class AlgorithmAnalysisApp {
 
         for(int x=0;x<temp.length;x++) arr[l+x]=temp[x];
     }
+
+    public static void quickSort(int[] arr, int low, int high){
+        if(low<high){
+            int pi = partition(arr,low,high);
+            quickSort(arr,low,pi-1);
+            quickSort(arr,pi+1,high);
+        }
+    }
+
+    static int partition(int[] arr,int low,int high){
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];arr[i]=arr[j];arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];arr[i+1]=arr[high];arr[high]=temp;
+        return i+1;
+    }
 }
