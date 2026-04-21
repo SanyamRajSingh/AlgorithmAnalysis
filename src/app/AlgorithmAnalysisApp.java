502eb88ee527c7ed6ffcdd7996f7e46bb89e89d1
SanyamRajSingh <sanyamrajsingh@gmail.com>
2026-04-21 19:53:57 +0530

    P6: Binary search implementation

--- src/app/AlgorithmAnalysisApp.java
+++ src/app/AlgorithmAnalysisApp.java
@@ -139,4 +139,15 @@ public class AlgorithmAnalysisApp {
         }
         return -1;
     }
+
+    public static int binarySearch(int[] arr,int key){
+        int l=0,h=arr.length-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(arr[m]==key) return m;
+            else if(arr[m]<key) l=m+1;
+            else h=m-1;
+        }
+        return -1;
+    }
 }
